768b392ffc79dac942d9bc6dc81ad177c7a9d8fc
alisson-moura <alisson.mo.moura@outlook.com.br>
2021-05-17 05:52:51 -0300

    adicionado e configurado swagger

--- src/infra/http/routes.ts
+++ src/infra/http/routes.ts
@@ -1,9 +1,15 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from '../../../swagger.json'
 import { restaurantsRouter } from '../../modules/restaurants/infra/http/restaurants.routes'
 import { accountsRouter } from '../../modules/accounts/infra/http/accounts.routes'
 
 const routes = Router()
 
+routes.use('/api-docs', swaggerUi.server)
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
+routes.use('/', (request: Request, response: Response) => response.redirect('/api-docs'))
 routes.use('/accounts', accountsRouter)
 routes.use('/restaurants', restaurantsRouter)
 
